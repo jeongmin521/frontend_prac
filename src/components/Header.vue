b4585b6f23f8a1051a15c21992a056f601ebd0b4
jeongmin <jeongmin0521@naver.com>
2024-05-28 20:28:14 +0900

    장바구니

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,7 @@
             <h4 class="text-white">사이트맵</h4>
             <ul class="list-unstyled">
               <li>
-              <router-link to="/" class="text-white">메인 화면</router-link>
+                <router-link to="/" class="text-white">메인 화면</router-link>
               </li>
               <li>
                 <router-link to="/login" class="text-white" v-if="!$store.state.account.id">로그인</router-link>
@@ -20,15 +20,18 @@
     </div>
     <div class="navbar navbar-dark bg-dark shadow-sm">
       <div class="container">
-        <a href="#" class="navbar-brand d-flex align-items-center">
+        <router-link to="/" class="navbar-brand d-flex align-items-center">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"
                stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2"
                viewBox="0 0 24 24">
             <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/>
             <circle cx="12" cy="13" r="4"/>
           </svg>
-          <strong>Album</strong>
-        </a>
+          <strong>Home</strong>
+        </router-link>
+        <router-link to="/cart" class="cart btn">
+          <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+        </router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
                 aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -57,4 +60,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+header .navbar .cart {
+  margin-left: auto;
+  color: white;
+}
 </style>
---
+++ src/pages/Cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="cart">
+    카트 페이지
+  </div>
+</template>
+
+<script>
+import {reactive} from "vue";
+export default {
+  setup(){
+    const state = reactive({
+    })
+    return {state}
+  }
+}
+</script>
+
+<style scoped>
+</style>
